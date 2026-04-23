7137e0eec9196893bf802236be825d420719a430
Salomon Montilla <thebilystaxx@gmail.com>
2026-04-23 20:25:00 +0000

    agrega conexiones

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "reimagined-goggles-jj7px9rq699qcp6px-3000.app.github.dev",
+        "localhost:3000"
+      ],
+    },
+  },
 };
 
 export default nextConfig;
